1bbe548155d7abe239a45c467d8a195351e569cd
Andrea Tinelli <tinelliandrea2@outlook.it>
2024-03-07 21:41:04 +0100

    Updated route name

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ export default {
   Login: "https://cas.unimi.it/login?service=https%3A%2F%2Funimia.unimi.it%2Fportal%2Fserver.pt",
   PostLogin: "https://cas.unimi.it/login",
   Home: "https://unimia.unimi.it/portal/server.pt/community/unimia/207/home",
-  Curriculum: "https://unimia.unimi.it/portal/server.pt/community/unimia/207/carriera"
+  Career: "https://unimia.unimi.it/portal/server.pt/community/unimia/207/carriera"
 };
